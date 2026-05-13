c14c4050f8a667da34639f52923780923fed623f
Aisyah Mokhtar <aimokhta@u80z06s02.42kl.edu.my>
2026-05-13 19:13:03 +0800

    feat: init - 42headers, include guards (#ifndef....#endif), preprocessor directives (#include ....)

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,1 +1,22 @@
-test
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aimokhta <aimokhta@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/13 15:25:52 by aimokhta          #+#    #+#             */
+/*   Updated: 2026/05/13 19:08:29 by aimokhta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av)
+{
+	
+};
+
+// i can do this
+// Allah can make the impossibles , possible. as how He did so for Musa, Hajar, Muhammad, Maryam
+// so can He for you too
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,1 +1,25 @@
-test
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aimokhta <aimokhta@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/13 18:57:20 by aimokhta          #+#    #+#             */
+/*   Updated: 2026/05/13 19:05:46 by aimokhta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RPN.hpp"
+
+/*
+	RPN =  Reverse Polish Notation
+	 A calculator for single digit numbers only,
+	 with the 4 operations (*, +, -, /)
+	* cannot be divided by 0
+*/
+
+int main(int ac, char **av)
+{
+	
+};
